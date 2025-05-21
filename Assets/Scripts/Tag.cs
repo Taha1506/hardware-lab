792b3051886a2206294b355681939a7e157c104d
mr-sina-imani <mr.sinaimani@gmail.com>
2025-05-21 17:02:25 +0330

    Update Tag.cs

--- Assets/Scripts/Tag.cs
+++ Assets/Scripts/Tag.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class Tag : MonoBehaviour
 {
+
+    private static float antennaDelayMean = 5e-6;
+    private static float antennaDelayStd = 1e-6;
     void Start()
     {
         GameObject[] anchors = GameObject.FindGameObjectsWithTag("Anchor");
 
         foreach (GameObject anchor in anchors)
         {
-            MessageBus.Instance.SendMessageDelayed(gameObject, anchor);
+            MessageBus.Instance.SendMessageDelayed(gameObject, anchor, antennaDelayMean, antennaDelayStd);
         }
     }
 }
